ADFC-Hamburg/fahrradfreundliche-schule

commit f806418b918c65e2f64cdbecf28314e85b06c2b2

feat(api): validate upload filetype

app/forms.py
@@ -31,7 +31,52 @@ class FileRequiredIfLinkDataMatch(FileRequired):
         if hasattr(field, 'link') and field.link.data in self.target:
             super().__call__(form, field)
 
+class MediatypeAllowed():
+    """Validates that the uploaded file(s) is allowed by a given list
+    of media types."""
+
+    def __init__(self, mediatypes:Iterable[str, ...], message=None):
+        self.message = message
+        self.allowed_types = mediatypes
+    
+    def __call__(self, form, field):
+        from magic import from_buffer as magic_from_buffer
+        from werkzeug.datastructures import FileStorage
+
+        field_data = (field.data,) if not isinstance(field.data, list) else field.data
+        if not (
+            all(isinstance(x, FileStorage) and x for x in field_data) and field_data
+        ):
+            return
+        
+        mediatypes = tuple(magic_from_buffer(f.read(), mime=True) for f in field_data)
+        if not all(t in self.allowed_types for t in mediatypes):
+            raise validators.StopValidation(
+                self.message or
+                ('File does not have an approved media type: '
+                + ', '.join(self.allowed_types))
+            )
+
 class ApplicationForm(FlaskForm):
+
+    #region Static methods
+    @staticmethod
+    def format_mediatypes_from_config(formconfig: dict) -> str:
+        """Reads allowed media types from a form configuration and
+        formats the list of allowed media types as a string."""
+        from mimetypes import guess_extension
+
+        mediatypes = formconfig[const.conf.UPLOADS_KEY][const.conf.MEDIATYPE_KEY]
+        extensions = tuple(guess_extension(t)[1:].upper() for t in mediatypes)
+        if not extensions:
+            return ''
+        if len(extensions) == 1:
+            return extensions[0]
+        return const.form.LIST_SEPARATOR_LAST.join((
+            const.form.LIST_SEPARATOR.join(extensions[:-1]),
+            extensions[-1]
+        ))
+    #endregion
 
     #region Input fields for personal and school information
     _FIELD_REQUIRED_VALIDATOR = validators.InputRequired(message=const.form.ERROR_REQUIRED)
@@ -183,9 +228,14 @@ class ApplicationForm(FlaskForm):
         message = const.form.ERROR_REQUIRED_FILE,
         target = (True, 1, '1'),
     )
+    _MEDIATYPE_VALIDATOR = MediatypeAllowed(
+        message = const.form.ERROR_MEDIATYPE % format_mediatypes_from_config(const.conf.FORM_DEFAULT),
+        mediatypes = const.conf.FORM_DEFAULT[const.conf.UPLOADS_KEY][const.conf.MEDIATYPE_KEY],
+    )
     _FILEFIELD_COMMON_ARGS = {
         'validators': [
             _FILE_MAYBE_REQUIRED_VALIDATOR,
+            _MEDIATYPE_VALIDATOR,
         ],
         'render_kw': {
             'accept': ', '.join(const.conf.FORM_DEFAULT[const.conf.UPLOADS_KEY][const.conf.MEDIATYPE_KEY])
@@ -248,6 +298,9 @@ class ApplicationForm(FlaskForm):
                     # Do not verify unused fields
                     fields[0].flags.skip_validation = True
                     fields[0].validate_choice = False
+
+            self._MEDIATYPE_VALIDATOR.mediatypes = formconfig[const.conf.UPLOADS_KEY][const.conf.MEDIATYPE_KEY]
+            self._MEDIATYPE_VALIDATOR.message = const.form.ERROR_MEDIATYPE % self.format_mediatypes_from_config(formconfig)
             for fields in self.questions.values():
                 fields[1].render_kw['accept'] = ', '.join(formconfig[const.conf.UPLOADS_KEY][const.conf.MEDIATYPE_KEY])
 

Dockerfile
@@ -10,6 +10,9 @@ LABEL org.opencontainers.image.description="Ein Webserver, über den Schulen sic
 ARG workdir='/opt/fahrradschule'
 WORKDIR "${workdir}"
 
+# Install required packages
+RUN apk add --no-cache libmagic
+
 # Update pip and install Python dependencies
 ENV PIP_ROOT_USER_ACTION=ignore
 COPY ./requirements.txt /abrechnungsformular/requirements.txt

app/const.py
@@ -104,8 +104,13 @@ class form(ABC):
     }
     CHOICE_YES = 'Ja'
     CHOICE_NO = 'Nein'
+
+    LIST_SEPARATOR = ', '
+    LIST_SEPARATOR_LAST = ' oder '
+
     ERROR_INVALID = 'Bitte gib einen gültigen Wert ein.'
     ERROR_LENGTH_ZIPCODE = 'Bitte gib genau '+str(ZIP_DIGITS)+' Ziffern ein.'
+    ERROR_MEDIATYPE = 'Bitte lade eine Datei im Format %s hoch.'
     ERROR_REQUIRED = 'Bitte fülle dieses Feld aus.'
     ERROR_REQUIRED_FILE = 'Bitte lade für jedes '+CHOICE_YES+' eine Datei hoch.'
     ERROR_REQUIRED_YESNO = 'Bitte wähle '+CHOICE_YES+' oder '+CHOICE_NO+' aus.'

commit 24bc16c0684880fc98c9de3f50384856d6396685

fix(api): stop replacing uploads with empty files
Fixes bug introduced in f806418b918c65e2f64cdbecf28314e85b06c2b2 which caused empty files to be saved instead of uploaded files due to filetype validation reading files and not resetting position in file object afterwards.

app/forms.py
@@ -52,13 +52,16 @@ class MediatypeAllowed():
         ):
             return
         
-        mediatypes = tuple(magic_from_buffer(f.read(), mime=True) for f in field_data)
-        if not all(t in self.allowed_types for t in mediatypes):
-            raise validators.StopValidation(
-                self.message or
-                ('File does not have an approved media type: '
-                + ', '.join(self.allowed_types))
-            )
+        for f in field_data:
+            previous_position = f.stream.tell()
+            mediatype = magic_from_buffer(f.read(), mime=True)
+            f.stream.seek(previous_position)
+            if not mediatype in self.allowed_types:
+                raise validators.StopValidation(
+                    self.message or
+                    ('File does not have an approved media type: '
+                    + ', '.join(self.allowed_types))
+                )
 
 class ApplicationForm(FlaskForm):
 